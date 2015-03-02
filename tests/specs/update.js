5ab8d4cb4e80855637a98ce4f4d83a5fb74e131e
Gregor Martynus <gregor@martynus.net>
2015-03-02 21:26:23 +0100

    test(update): store.update(array) & store.update(array, change)

--- tests/specs/update.js
+++ tests/specs/update.js
@@ -106,3 +106,139 @@ test('store.update(object)', function (t) {
     t.is(object.foo, 'bar', 'resolves with properties')
   })
 })
+
+test('store.update(array)', function (t) {
+  t.plan(6)
+
+  var db = dbFactory()
+  var store = new Store(db)
+
+  store.add([
+    { id: '1', foo: 'foo', bar: 'foo'},
+    { id: '2', foo: 'bar'}
+  ])
+
+  .then(function () {
+    return store.update([
+      { id: '1', bar: 'baz'},
+      { id: '2', bar: 'baz'}
+    ])
+  })
+
+  .then(function (objects) {
+    t.is(objects[0].id, '1')
+    t.is(objects[0].foo, 'foo')
+    t.is(objects[0].bar, 'baz')
+
+    t.is(objects[1].id, '2')
+    t.is(objects[1].foo, 'bar')
+    t.is(objects[1].bar, 'baz')
+  })
+})
+
+// blocked by https://github.com/boennemann/pouchdb-hoodie-api/issues/8
+test.skip('store.update(array) with non-existent object', function (t) {
+  t.plan(3)
+
+  var db = dbFactory()
+  var store = new Store(db)
+
+  store.add({ id: 'exists'})
+
+  .then(function () {
+    return store.update([
+      { id: 'exists', foo: 'bar'},
+      { id: 'unknown', foo: 'baz'}
+    ])
+  })
+
+  .then(function (objects) {
+    t.is(objects[0].id, 'exists')
+    t.is(objects[0].foo, 'bar')
+
+    t.is(objects[1].status, 400)
+  })
+})
+
+// https://github.com/boennemann/pouchdb-hoodie-api/issues/9
+test.skip('store.update(array) with invalid objects', function (t) {
+  t.plan(3)
+
+  var db = dbFactory()
+  var store = new Store(db)
+
+  store.add(
+    { id: 'exists' },
+    { id: 'foo' }
+  )
+
+  .then(function () {
+    return store.update([
+      { id: 'exists', foo: 'bar'},
+      'foo'
+    ])
+  })
+
+  .then(function (objects) {
+    t.is(objects[0].id, 'exists')
+    t.is(objects[0].foo, 'bar')
+
+    t.is(objects[1].status, 400)
+  })
+})
+
+test('store.update(array, changedProperties)', function (t) {
+  t.plan(6)
+
+  var db = dbFactory()
+  var store = new Store(db)
+
+  store.add([
+    { id: '1', foo: 'foo', bar: 'foo'},
+    { id: '2', foo: 'bar'}
+  ])
+
+  .then(function () {
+    return store.update([{id: '1'}, '2'], {
+      bar: 'baz'
+    })
+  })
+
+  .then(function (objects) {
+    t.is(objects[0].id, '1')
+    t.is(objects[0].foo, 'foo')
+    t.is(objects[0].bar, 'baz')
+
+    t.is(objects[1].id, '2')
+    t.is(objects[1].foo, 'bar')
+    t.is(objects[1].bar, 'baz')
+  })
+})
+
+test('store.update(array, updateFunction)', function (t) {
+  t.plan(6)
+
+  var db = dbFactory()
+  var store = new Store(db)
+
+  store.add([
+    { id: '1', foo: 'foo', bar: 'foo'},
+    { id: '2', foo: 'bar'}
+  ])
+
+  .then(function () {
+    return store.update(['1', '2'], function (object) {
+      object.bar = object.id + 'baz'
+    })
+  })
+
+  .then(function (objects) {
+    t.is(objects[0].id, '1')
+    t.is(objects[0].foo, 'foo')
+    t.is(objects[0].bar, '1baz')
+
+    t.is(objects[1].id, '2')
+    t.is(objects[1].foo, 'bar')
+    t.is(objects[1].bar, '2baz')
+  })
+})
