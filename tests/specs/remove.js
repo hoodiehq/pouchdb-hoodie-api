038711e34c38f14d57d626a786c5ab434af02f13
Gregor Martynus <gregor@martynus.net>
2015-03-06 16:44:32 +0100

    test(remove): store.remove(array)

--- tests/specs/remove.js
+++ tests/specs/remove.js
@@ -40,13 +40,14 @@ test('removes existing by id', function (t) {
 })
 
 test('removes existing by object', function (t) {
-  t.plan(2)
+  t.plan(3)
 
   var db = dbFactory()
   var store = new Store(db)
 
   store.add({
-    id: 'foo'
+    id: 'foo',
+    foo: 'bar'
   })
 
   .then(function () {
@@ -55,12 +56,13 @@ test('removes existing by object', function (t) {
 
   .then(function (object) {
     t.is(object.id, 'foo', 'resolves value')
+    t.is(object.foo, 'bar', 'resolves value')
 
     return store.find('foo')
   })
 
-  .catch(function (err) {
-    t.ok(err instanceof Error, 'rejects error')
+  .catch(function (error) {
+    t.is(error.status, 404, 'rejects with 404 error')
   })
 })
 
@@ -80,5 +82,35 @@ test('fails for non-existing', function (t) {
 
   .catch(function (err) {
     t.ok(err instanceof Error, 'rejects error')
+  })
+})
+
+test('store.remove(array) removes existing, returns error for non-existing', function (t) {
+  t.plan(7)
+
+  var db = dbFactory()
+  var store = new Store(db)
+
+  store.add([
+    { id: 'exists1', foo: 'bar' },
+    { id: 'exists2', foo: 'baz' }
+  ])
+
+  .then(function () {
+    return store.remove([
+      'exists1',
+      { id: 'exists2' },
+      'unknown'
+    ])
+  })
+
+  .then(function (objects) {
+    t.is(objects[0].id, 'exists1', 'resolves with value for existing')
+    t.is(objects[0].foo, 'bar', 'resolves with value for existing')
+    t.is(parseInt(objects[0]._rev, 10), 2, 'resolves with revision 2')
+    t.is(objects[1].id, 'exists2', 'resolves with value for existing')
+    t.is(objects[1].foo, 'baz', 'resolves with value for existing')
+    t.is(parseInt(objects[1]._rev, 10), 2, 'resolves with revision 2')
+    t.is(objects[2].status, 404, 'resolves with 404 error for non-existing')
   })
 })
