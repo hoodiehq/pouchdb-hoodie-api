1f8934ec492424d4b7f21cec7fd446dacc38e53f
Gregor Martynus <gregor@martynus.net>
2015-04-19 11:22:18 +0200

    feat: export `.hoodieApi()` factory method

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 'use strict'
 
-module.exports = Store
+module.exports = { hoodieApi: hoodieApi }
 
-function Store (db) {
-  if (!(this instanceof Store)) return new Store(db)
-  if (typeof db !== 'object' || typeof db.adapter !== 'string') throw new Error('Must pass a PouchDB')
+function hoodieApi () {
+  var api = {
+    db: this,
+    utils: {
+      Promise: this.constructor.utils.Promise,
+      Errors: this.constructor.Errors
+    }
+  }
 
-  this.db = db
-  this.PouchDB = db.constructor
+  api.add = require('./lib/add').bind(api)
+  api.find = require('./lib/find').bind(api)
+  api.findAll = require('./lib/find-all').bind(api)
+  api.findOrAdd = require('./lib/find-or-add').bind(api)
+  api.update = require('./lib/update').bind(api)
+  api.updateOrAdd = require('./lib/update-or-add').bind(api)
+  api.updateAll = require('./lib/update-all').bind(api)
+  api.remove = require('./lib/remove').bind(api)
 
-  this.add = require('./lib/add').bind(this)
-  this.find = require('./lib/find').bind(this)
-  this.findAll = require('./lib/find-all').bind(this)
-  this.findOrAdd = require('./lib/find-or-add').bind(this)
-  this.update = require('./lib/update').bind(this)
-  this.updateOrAdd = require('./lib/update-or-add').bind(this)
-  this.updateAll = require('./lib/update-all').bind(this)
-  this.remove = require('./lib/remove').bind(this)
+  return api
 }
