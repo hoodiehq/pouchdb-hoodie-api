069a244399343a0b1b02a4e0ed98e3ba8ec0023a
Stephan Bönnemann <stephan@boennemann.me>
2015-04-20 12:14:37 +0200

    chore: build and test prebuilt version

--- tests/utils/db.js
+++ tests/utils/db.js
@@ -1,19 +1,18 @@
 'use strict'
 
 var PouchDB = process.browser ? global.PouchDB : require('pouchdb')
-var hoodieApi = require('../../')
 var uuid = PouchDB.utils.uuid
+
+if (!PouchDB.prototype.hoodieApi) PouchDB.plugin(require('../../'))
+
+var options = process.browser ? {
+  adapter: 'memory'
+} : {
+  db: require('memdown')
+}
 
 module.exports = function (name) {
   name = name || uuid(10)
-
-  var options = process.browser ? {
-    adapter: 'memory'
-  } : {
-    db: require('memdown')
-  }
-
-  PouchDB.plugin(hoodieApi)
 
   return new PouchDB(name, options)
 }
