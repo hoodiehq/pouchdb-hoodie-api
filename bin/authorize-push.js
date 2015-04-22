a379637bb2eedb157475c6972080e43e4cb904fd
Stephan Bönnemann <stephan@boennemann.me>
2015-04-22 18:00:59 +0200

    chore(authorize-push): make node 0.10 compatible

--- bin/authorize-push.js
+++ bin/authorize-push.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var exec = require('child_process').execSync
+var exec = require('child_process').exec
 
 var GH_TOKEN = process.env.GH_TOKEN
 var repo = require('../package.json').repository.url
@@ -9,6 +9,8 @@ if (!(process.env.CI && GH_TOKEN && repo)) {
   process.exit(1)
 }
 
-exec('git remote set-url origin ' + repo.replace('https://', 'https://' + GH_TOKEN + '@'))
-exec('git config user.email stephan@thehoodiefirm.com')
-exec('git config user.name "Hoodie Bot"')
+exec(
+  'git remote set-url origin ' + repo.replace('https://', 'https://' + GH_TOKEN + '@') + ' && '
+  'git config user.email stephan@thehoodiefirm.com && '
+  'git config user.name "Hoodie Bot"'
+)
