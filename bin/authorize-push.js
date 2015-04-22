1347d77e7582a2cf7f67c19cf44ad6554628ddab
Stephan Bönnemann <stephan@boennemann.me>
2015-04-22 15:16:19 +0200

    chore(gh-pages): add script to setup git on CI

---
+++ bin/authorize-push.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var exec = require('child_process').execSync
+
+var GH_TOKEN = process.env.GH_TOKEN
+var repo = require('../package.json').repository.url
+
+if (!(process.env.CI && GH_TOKEN && repo)) {
+  process.exit(1)
+}
+
+exec('git remote set-url origin ' + repo.replace('https://', 'https://' + GH_TOKEN + '@'))
+exec('git config user.email stephan@thehoodiefirm.com')
+exec('git config user.name "Hoodie Bot"')
