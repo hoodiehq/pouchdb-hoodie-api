c1fab914b98545476e207b990a56591703776b79
Nick Hehr <headhipster@hipsterbrown.com>
2015-07-03 15:41:37 -0400

    test(factory): adds optional EventEmitter instance

--- tests/specs/factory.js
+++ tests/specs/factory.js
@@ -2,6 +2,7 @@
 
 var test = require('tape')
 var dbFactory = require('../utils/db')
+var EventEmitter = require('events').EventEmitter
 
 test('returns hoodie.store-inspired API', function (t) {
   t.plan(3)
@@ -14,4 +15,17 @@ test('returns hoodie.store-inspired API', function (t) {
 
   t.is(typeof store, 'object', 'is object')
   t.is(store.db, db, 'exposes db')
+})
+
+test('creates store with custom EventEmitter instance', function (t) {
+  t.plan(1)
+
+  var db = dbFactory()
+  var emitter = new EventEmitter()
+  var store = db.hoodieApi({emitter: emitter})
+
+  store.on('foo', function () {
+    t.ok('emitter used from options')
+  })
+  emitter.emit('foo')
 })
