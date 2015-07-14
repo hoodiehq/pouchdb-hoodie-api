e956764c75991a176670cb267fdc02e8f1dc9be0
Tobias <tobias.hartmann@sitewards.com>
2015-07-14 07:51:48 +0200

    test(clear): initial version

---
+++ tests/specs/clear.js
@@ -0,0 +1,44 @@
+'use strict'
+
+var test = require('tape')
+
+var dbFactory = require('../utils/db')
+
+test('has "clear" method', function (test) {
+  test.plan(1)
+
+  var db = dbFactory()
+  var store = db.hoodieApi()
+
+  test.is(typeof store.clear, 'function', 'has method')
+})
+
+test('removes existing db', function (t) {
+  t.plan(2)
+
+  var db = dbFactory()
+  var store = db.hoodieApi()
+
+  var localObj1 = {
+    _id: 'cleanTest',
+    cnt: 'cleanTest dummy doc'
+  }
+  store.on('clear', function () {
+    t.pass("store 'clear' event was triggered")
+  })
+
+  store.add(localObj1)
+
+    .then(function () {
+      return store.clear()
+    })
+
+    .then(function () {
+      return db.get('cleanTest')
+    })
+
+    .catch(function (error) {
+      t.equal(error.status, 404, 'doc removed after store.clear')
+    })
+})
+
