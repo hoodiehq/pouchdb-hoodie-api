111d37eb75472937f820f903db70fc5534597c80
Jan Pawellek <jan@janpawellek.de>
2016-01-24 16:19:15 +0100

    fix: Not Found error

--- helpers/find-one.js
+++ helpers/find-one.js
@@ -5,5 +5,18 @@ var toObject = require('../utils/to-object')
 
 module.exports = function findOne (idOrObject) {
   var id = toId(idOrObject)
-  return this.get(id).then(toObject)
+  return this.get(id)
+
+  .then(toObject)
+
+  .catch(function (error) {
+    if (error.status === 404) {
+      var missing = new Error('Object with id "' + id + '" is missing')
+      missing.name = 'Not found'
+      missing.status = 404
+      throw missing
+    } else {
+      throw error
+    }
+  })
 }
