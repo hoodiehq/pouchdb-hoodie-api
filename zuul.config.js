59761815390326dfea7cef19b8d8ec89fa6dd725
Gregor <gregor@martynus.net>
2016-09-01 13:59:57 -0700

    chore(zuul): load PouchDB from new location

--- zuul.config.js
+++ zuul.config.js
@@ -1,7 +1,6 @@
 var zuulConfig = module.exports = require('hoodie-zuul-config')
 
 zuulConfig.scripts = [
-  './node_modules/pouchdb/dist/pouchdb.js',
-  './node_modules/pouchdb/dist/pouchdb.memory.js',
+  './dist/pouchdb-memory.js',
   './dist/pouchdb-hoodie-api.js'
 ]
