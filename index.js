20d3a76cf827979222569d0a86d42dc401f14461
Gregor <gregor@martynus.net>
2017-02-25 23:15:11 -0800

    refactor: this no more

--- index.js
+++ index.js
@@ -7,28 +7,33 @@ var startListenToChanges = require('./lib/helpers/start-listen-to-changes')
 
 function hoodieApi (options) {
   var state = {
-    emitter: options && options.emitter || new EventEmitter()
+    emitter: options && options.emitter || new EventEmitter(),
+    db: this
   }
 
-  state.emitter.once('newListener', startListenToChanges.bind(this, state))
+  state.emitter.once('newListener', startListenToChanges.bind(null, state))
 
-  return {
-    db: this,
-    add: require('./lib/add').bind(this, null),
-    find: require('./lib/find').bind(this, null),
-    findAll: require('./lib/find-all').bind(this, null),
-    findOrAdd: require('./lib/find-or-add').bind(this, state, null),
-    update: require('./lib/update').bind(this, null),
-    updateOrAdd: require('./lib/update-or-add').bind(this, null),
-    updateAll: require('./lib/update-all').bind(this, null),
-    remove: require('./lib/remove').bind(this, null),
-    removeAll: require('./lib/remove-all').bind(this, null),
-    withIdPrefix: require('./lib/with-id-prefix').bind(this, state),
-    on: require('./lib/on').bind(this, state),
-    one: require('./lib/one').bind(this, state),
-    off: require('./lib/off').bind(this, state),
-    clear: require('./lib/clear').bind(this, state)
+  var api = {
+    db: state.db,
+    add: require('./lib/add').bind(null, state, null),
+    find: require('./lib/find').bind(null, state, null),
+    findAll: require('./lib/find-all').bind(null, state, null),
+    findOrAdd: require('./lib/find-or-add').bind(null, state, null),
+    update: require('./lib/update').bind(null, state, null),
+    updateOrAdd: require('./lib/update-or-add').bind(null, state, null),
+    updateAll: require('./lib/update-all').bind(null, state, null),
+    remove: require('./lib/remove').bind(null, state, null),
+    removeAll: require('./lib/remove-all').bind(null, state, null),
+    withIdPrefix: require('./lib/with-id-prefix').bind(null, state),
+    on: require('./lib/on').bind(null, state),
+    one: require('./lib/one').bind(null, state),
+    off: require('./lib/off').bind(null, state),
+    clear: require('./lib/clear').bind(null, state)
   }
+
+  state.api = api
+
+  return api
 }
 
 /* istanbul ignore next */
